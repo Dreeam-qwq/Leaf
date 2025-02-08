14374a73fe827abdb9798da6c5ed1eac1553b1a7
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-02-08 04:26:57 -0500

    cleanup

--- leaf-server/src/main/java/org/dreeam/leaf/util/item/ItemStackStripper.java
+++ leaf-server/src/main/java/org/dreeam/leaf/util/item/ItemStackStripper.java
@@ -3,8 +3,10 @@ package org.dreeam.leaf.util.item;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.item.ItemStack;
-import org.dreeam.leaf.config.modules.misc.HideItemComponent;
+import org.dreeam.leaf.config.modules.gameplay.HideItemComponent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -16,16 +18,43 @@ import java.util.Objects;
 public class ItemStackStripper {
 
     public static ItemStack strip(final ItemStack itemStack, final boolean copy) {
-        if (!HideItemComponent.enabled ||
-            itemStack.isEmpty() || itemStack.getComponentsPatch().isEmpty()) return itemStack;
+        if (!HideItemComponent.enabled || itemStack.isEmpty() || itemStack.getComponentsPatch().isEmpty())
+            return itemStack;
 
         final ItemStack copied = copy ? itemStack.copy() : itemStack;
+
         // Remove specified types
         for (DataComponentType<?> type : HideItemComponent.hiddenTypes) {
             // Only remove, no others
             copied.remove(type);
         }
+
         return copied;
+    }
+
+    public static List<ItemStack> strip(final List<ItemStack> itemStacks, final boolean copy) {
+        if (!HideItemComponent.enabled) return itemStacks;
+
+        final List<ItemStack> copiedItems = new ArrayList<>();
+
+        for (ItemStack itemStack : itemStacks) {
+            if (itemStack.isEmpty() || itemStack.getComponentsPatch().isEmpty()) {
+                copiedItems.add(itemStack);
+                continue;
+            }
+
+            final ItemStack copied = copy ? itemStack.copy() : itemStack;
+
+            // Remove specified types
+            for (DataComponentType<?> type : HideItemComponent.hiddenTypes) {
+                // Only remove, no others
+                copied.remove(type);
+            }
+
+            copiedItems.add(copied);
+        }
+
+        return copiedItems;
     }
 
     /**
@@ -37,7 +66,9 @@ public class ItemStackStripper {
                 left.is(right.getItem()) && left.getCount() == right.getCount() &&
                     (left.isEmpty() && right.isEmpty() || Objects.equals(strip(left.getComponents()), strip(right.getComponents())))
             );
-        } else return ItemStack.matches(left, right);
+        }
+
+        return ItemStack.matches(left, right);
     }
 
     /**
@@ -46,5 +77,4 @@ public class ItemStackStripper {
     private static DataComponentMap strip(final DataComponentMap map) {
         return map.filter(c -> !HideItemComponent.hiddenTypes.contains(c));
     }
-
 }
